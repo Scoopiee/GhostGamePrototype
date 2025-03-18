229abb6305b23f86b09bb63ef8279bdf80e7f915
Scoopiee <aidanmccarthy614@outlook.com>
2025-03-18 15:53:17 +0000

    added the demo player to level scene for collider/interactable testing

---
+++ Assets/_Scripts/TempPlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    
+    private InputAction _moveAction;
+    private InputAction _interactAction;
+    
+    private Vector2 _moveInput = Vector2.zero;
+    private bool _interact;
+    
+    private Rigidbody2D _rb;
+    
+    private float _yDirection;
+    private float _xDirection;
+    
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        // Check for WASD (or controller) movement
+        _moveAction = InputSystem.actions.FindAction("Move");
+        // Check for E key (or north button on controller) press
+        _interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    private void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+
+        if (_interactAction.triggered)
+        {
+            Interact();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * speed;
+    }
+
+    private void Interact()
+    {
+        Debug.Log("Interact");
+        // TODO: Make an interact function which hides a player if hiding spot in certain range
+    }
+}
