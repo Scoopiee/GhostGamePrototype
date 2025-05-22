88eaf0342436481b1df7f60f2fe4f147dc795073
MynameisTom04 <thomas@hurleyhome.co.uk>
2025-05-23 00:27:19 +0100

    Change Pause Menu again

--- Assets/_Scripts/UI/PauseMenu.cs
+++ Assets/_Scripts/UI/PauseMenu.cs
@@ -44,6 +44,6 @@ public class PauseMenu : MonoBehaviour
     public void GoToMainMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
