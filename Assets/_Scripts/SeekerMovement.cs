fbf256be0ec21385229df47871741b557f1fe466
Scoopiee <aidanmccarthy614@outlook.com>
2025-05-24 20:48:05 +0100

    added basic line of sight functionality to seekers and they can now move too

--- Assets/_Scripts/SeekerMovement.cs
+++ Assets/_Scripts/SeekerMovement.cs
@@ -1,53 +1,72 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class SeekerMovement : MonoBehaviour
 {
-    [SerializeField] private float detectionRadius = 5f; // Distance to detect the player
-    private CircleCollider2D circleCollider; // Circle collider to visualize the detection radius
-    private bool playerDetected = false; // Flag to check if the player is detected
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private bool playerDetected = false; // Flag to check if the player is detected
+    [SerializeField] private float detectionRadius = 5f; // Radius within which the seeker can detect the player
+    [SerializeField] private GameObject player; // Radius within which the seeker can detect the player
+    private Vector3 directionToPlayer;
+    float distanceToPlayer; 
+
 
     void Awake()
     {
-        circleCollider = gameObject.GetComponent<CircleCollider2D>();
+
     }
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if (circleCollider != null) // set its radius to the detection radius *not permanent, just for testing purposes*
+        directionToPlayer = (player.transform.position - transform.position).normalized;
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        playerDetected = false; // assume player is not detected at the start of each frame
+       
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, directionToPlayer, detectionRadius); // shoots a beam and returns a list of colliders
+
+        foreach (var hit in hits)
         {
-            circleCollider.radius = detectionRadius;
+            if (hit.collider != null && hit.collider.gameObject == this.gameObject) // bypass self
+                continue;
+
+            if (hit.collider.CompareTag("Player"))
+            {
+                playerDetected = true;
+            }
+
+            break; //exit the loop after first hit is checked to be a player
         }
-        
-        if (playerDetected) // If the player is detected
+
+        if (playerDetected && distanceToPlayer >= 1.5f)  // if the player is detected and within a certain distance (so it doesn't run into player)
         {
-            MoveToPlayer(); // Move towards the player
+            MoveToPlayer();
         }
+
+        Debug.DrawRay(transform.position, directionToPlayer * detectionRadius, Color.red); // Draw a ray in the editor for debugging
+        Debug.Log($"Distance to player: {distanceToPlayer}, Player Detected: {playerDetected} Raycast Hit: {hits}");
     }
+
     void MoveToPlayer() // Move to player if the player has been 'seen' by the seeker
     {
-
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        transform.position += (Vector3)(direction * moveSpeed * Time.deltaTime);
     }
-
-    private void OnTriggerEnter2D(Collider2D other) // Check if the player is within the detection radius
+    
+    void OnDrawGizmosSelected() // draw raycast in editor 
     {
-        if (other.CompareTag("Player")) // If the player is detected
+       if (player != null)
         {
-            playerDetected = true; // Set the player detected flag to true
-        }
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            Vector3 endPosition = transform.position + direction * detectionRadius;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position, endPosition);
+        } 
     }
-    private void OnTriggerExit2D(Collider2D other) // Check if the player is outside the detection radius
-    {
-        if (other.CompareTag("Player")) // If the player is no longer detected
-        {
-            playerDetected = false; // Set the player detected flag to false
-        }
-    }
+    
 }
-/* TODO: Movement to player 
-         Raycasting to check if the player is in line of sight (may require layermask stuff)
-         Catch the player if they are touched by the seeker
+/* TODO: Catch the player if they are touched by the seeker
 */
