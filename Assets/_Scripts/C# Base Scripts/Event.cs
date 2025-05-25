efb7a2d9e4b3b87c97540b376d3d373c2347eec8
MynameisTom04 <thomas@hurleyhome.co.uk>
2025-05-25 17:19:39 +0100

    Added a name to events to be called more specifically when needed

--- Assets/_Scripts/C# Base Scripts/Event.cs
+++ Assets/_Scripts/C# Base Scripts/Event.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Event 
+public class Event
 {
+    [SerializeField] private string eventName;
     [SerializeField] private string text;
     [SerializeField] private float eventDuration;
     
+    public string EventName => eventName;
     public string Text => text;
     public float EventDuration => eventDuration;
 }
