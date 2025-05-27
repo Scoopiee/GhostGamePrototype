c55d580b37a09af0ac1a088f657e6ba47ac55c56
MynameisTom04 <thomas@hurleyhome.co.uk>
2025-05-27 11:18:37 +0100

    Made adjustments to the way soul orb works

--- Assets/_Scripts/C# Base Scripts/SoulOrbData.cs
+++ Assets/_Scripts/C# Base Scripts/SoulOrbData.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SoulOrbData", menuName = "Scriptable Objects/SoulOrbData")]
 public class SoulOrbData : ScriptableObject
 {
-    public string soulOrbName;
+    public string ghostName;
     public int scoreValue;
     // Add any value which is needed to be kept in inventory after item is removed
+    // public Color spriteColour;
+    // public Sprite soulOrbSprite;
 }
