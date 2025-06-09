3688e64eb918f75d12edbbe7e174de46463257de
MynameisTom04 <thomas@hurleyhome.co.uk>
2025-06-09 11:59:50 +0100

    Added vertical spacing to inventory panel instead of manually moving soul data panels

--- Assets/_Scripts/UI/Inventory.cs
+++ Assets/_Scripts/UI/Inventory.cs
@@ -10,10 +10,6 @@ public class Inventory : MonoBehaviour
     [SerializeField] private GameObject inventoryPanel;
     [SerializeField] private GameObject soulOrbToCollectPanelPrefab;
 
-    [SerializeField] private float verticalSpacing;
-    [SerializeField] private float startYOffset;
-    [SerializeField] private float startXOffset;
-
     private List<SoulOrbData> _soulsToCollect = new List<SoulOrbData>();
 
     private GameObject _tempPanel;
@@ -21,18 +17,14 @@ public class Inventory : MonoBehaviour
 
     private void Start()
     {
-        float currentY = startYOffset;
         
         foreach (SoulOrb child in sceneSoulList.GetComponentsInChildren<SoulOrb>())
         {
             _soulsToCollect.Add(child.soulOrbData); // Add children of the list of souls to the list needed to collect
             _tempPanel = Instantiate(soulOrbToCollectPanelPrefab, gameObject.transform); // Create a new panel for each soul
-            _rt = _tempPanel.GetComponent<RectTransform>(); 
-            _rt.anchoredPosition = new Vector2(startXOffset, -currentY); // Move the position of the soul panel
 
             _tempPanel.GetComponent<SoulOrbToCollectInfo>().AddData(child.soulOrbData);
             
-            currentY += _rt.sizeDelta.y + verticalSpacing;
         }
         
         UIManager.instance.soulOrbsManager.InitializeDictionary(gameObject);
